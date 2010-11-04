f8b5244c24ab5210f1d7546f1d1042b79b36b1b7
Daniel Norberg <daniel.norberg@gmail.com>
2010-11-04 01:12:57 +0100

    Made test launcher script launch coordinators

--- test/launcher.py
+++ test/launcher.py
@@ -1,6 +1,7 @@
 import yaml
 import subprocess
 import os, sys
+import time
 
 import paths
 sys.path.insert(0, paths.home)
@@ -9,14 +10,23 @@ from hokanjo.configuration import Configuration
 def launch():
 	"""docstring for launch"""
 	os.chdir(paths.home)
-	cfg = Configuration(yaml.load(open(paths.path('test/config.yaml'))))
-	cmds = ['python hokanjo/nodeserver.py -id %s -f var/data/%s.tch -cfg test/config.yaml &> var/log/%s.log' % (node.id, node.id, node.id) for node in cfg.active_deployment.nodes.itervalues()]
-	processes = [subprocess.Popen(cmd, shell=True) for cmd in cmds]
+	cfg = Configuration(yaml.load(open(paths.path('test/local_cluster.yaml'))))
+	coordinator_cmds = ['python bin/hokanjo-coordinator -id %s -cfg test/local_cluster.yaml &> var/log/coordinator-%s.log' % (coordinator.id, coordinator.id) for coordinator in cfg.coordinators.itervalues()]
+	node_cmds = ['python bin/hokanjo-node -id %s -c localhost 4701 &> var/log/node-%s.log' % (node.id, node.id) for node in cfg.active_deployment.nodes.itervalues()]
+	coordinator_processes = [subprocess.Popen(cmd, shell=True) for cmd in coordinator_cmds]
+	time.sleep(1)
+	node_processes = [subprocess.Popen(cmd, shell=True) for cmd in node_cmds]
 	try:
 		while True:
 			raw_input()
 	except:
 		pass
+
+	for process in coordinator_processes:
+		process.terminate()
+	for process in node_processes:
+		process.terminate()
+
 	print 'Exiting...'
 
 if __name__ == '__main__':
