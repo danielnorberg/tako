6199854a9551d35aa04604575dce37cfa666c1ed
Daniel Norberg <daniel.norberg@gmail.com>
2010-12-21 12:25:49 +0100

    synclessified http server

--- tako/httpserver.py
+++ tako/httpserver.py
@@ -1,6 +1,6 @@
 import logging
-import gevent
-from gevent.wsgi import WSGIServer
+from syncless import coio
+from syncless import wsgi
 
 class BadRequest(object):
 	"""docstring for BadRequest"""
@@ -42,6 +42,42 @@ class HttpServer(object):
 
 	def serve(self):
 		"""docstring for serve"""
+		def _handle(env, start_response):
+			return self.handle_request(env, start_response)
 		logging.info('Listening on port %d', self.port)
-		self.wsgi_server = WSGIServer(('', self.port), self.handle_request)
-		self.wsgi_server.serve_forever()
+		wsgi.RunHttpServer(_handle, ('', self.port))
+
+from utils.testcase import TestCase
+class TestHttpServer(TestCase):
+
+	class DummyHttpServer(HttpServer):
+		"""docstring for DummyHttpServer"""
+		def __init__(self, port):
+			super(TestHttpServer.DummyHttpServer, self).__init__()
+			self.handlers = (
+				('/', {'GET':self.GET}),
+			)
+			self.port = port
+
+		def GET(self, start_response, path, body, env):
+			"""docstring for GET"""
+			start_response("200 OK", [('Content-Type', 'text/html')])
+			return ["TestHttpServer"]
+
+	def testServer(self):
+		import urllib
+		from syncless import patch
+		patch.patch_socket()
+		"""docstring for testServer"""
+		s = TestHttpServer.DummyHttpServer(4711)
+		t = coio.stackless.tasklet(s.serve)()
+		coio.stackless.schedule()
+		stream = urllib.urlopen('http://127.0.0.1:4711/')
+		body = stream.read()
+		stream.close()
+		assert body == "TestHttpServer"
+		t.kill()
+
+if __name__ == '__main__':
+	import unittest
+	unittest.main()
--- tako/paths.py
+++ tako/paths.py
@@ -1,8 +1,23 @@
-import os
-home = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-data = os.path.join(home, 'var', 'data')
-log = os.path.join(home, 'var', 'log')
+import os, sys
+
+def parent(path):
+	assert path
+	return os.path.dirname(path)
+
+def find_root(root_name):
+	root = parent(os.path.abspath(__file__))
+	while os.path.basename(root) != root_name:
+		if root == '/':
+			raise Exception('Project root not found!')
+	 	root = parent(root)
+	return root
 
 def path(_path):
 	"""docstring for path"""
 	return os.path.join(home, _path)
+
+home = parent(find_root('tako'))
+data = os.path.join(home, 'var', 'data')
+log = os.path.join(home, 'var', 'log')
+
+sys.path.append(home)
