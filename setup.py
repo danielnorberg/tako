8ea83457738064f32db4b1b2bcf3e8b192846d72
Daniel Norberg <daniel.norberg@gmail.com>
2011-03-17 02:25:53 +0100

    version bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='tako',
-    version='0.0.11',
+    version='0.0.12',
     packages=['tako', 'tako.utils'],
     scripts=[
         'bin/tako-cluster',
